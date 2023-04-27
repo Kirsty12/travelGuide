0cf13d847e4522c2e5bf5ad14750cef0a58b3b73
Kirsty Ratchford <Kirsty.Ratchford@nhs.net>
2023-04-27 14:27:16 +0100

    commit

--- Js.js
+++ Js.js
@@ -16,12 +16,14 @@ if (searchInput !== ""){
 }
 
 //pass base url and search input to getCoords to get data from api. 
-/*const getCoords = async (baseUrl, searchInput) => {
+    const getCoords = async (baseUrl, searchInput) => {
     const URL = `${baseUrl}${searchInput}`; 
     console.log(URL); 
 
     //fetch call to get data from api
     const results = await fetch(URL);
+        
+
     try{
         //convert data to json format
         const data = await results.json(); 
@@ -33,35 +35,12 @@ if (searchInput !== ""){
         console.log(error); 
     }
 
-}; 
-*/
 
-const getCoords = async (baseUrl, searchInput) => {
-    const URL = `${baseUrl}${searchInput}`; 
-    console.log(URL); 
-
-    //fetch call to get data from api
-    fetch(URL).then((data) =>{
-        return data.json(); 
-    }).then((objectData)=>{
-        console.log(objectData[0].capital); 
-
-        let tableData=""; 
-        objectData.map((values)=>{
-            tableData =`<li>${values.capital}</li>`;
-        }); 
-        document.getElementById("displyInfo").innerHTML=tableData;
-    })
 
 }; 
 
-function displayInfo(data){
-    data.forEach(country => {
-        const markup = `<li>${country.capital}</li>`;
 
-        document.getElementById('countriesInfo').innerHTML = markup;
-    })
-}
+
 
 
 
